ce1a4411ed4a47a49c045f682039dd85b7c4b90f
Rohit Nerkar <85748562+rohitnerkar@users.noreply.github.com>
2024-08-30 14:28:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,7 @@ const app = express();
 
 connectDB();
 
-const corsOptions = {
-  origin: 'https://role-based-admin-panel-frontend.onrender.com',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
