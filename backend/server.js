e63bc57a26a67259d5ff4f4c5054916cecc64810
Rohit Nerkar <rohitnerkar1997@gmail.com>
2024-08-30 14:47:59 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
 dotenv.config();
@@ -11,7 +12,8 @@ const app = express();
 connectDB();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "500mb" }));
+app.use(bodyParser.urlencoded({ limit: "500mb", extended: true}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
