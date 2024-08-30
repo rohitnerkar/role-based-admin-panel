f4e0716d604cbbfa666e546abe5d3f01bd8ce06f
Rohit Nerkar <85748562+rohitnerkar@users.noreply.github.com>
2024-08-30 15:04:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,7 @@ const app = express();
 
 connectDB();
 
-// app.use(cors());
-
-app.use(cors({
-  origin: 'https://role-based-admin-panel-frontend.onrender.com', // Allow requests from this origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these HTTP methods
-  credentials: true, // Allow cookies to be sent with the requests
-}));
+app.use(cors());
 
 app.use(express.json({ limit: "500mb" }));
 app.use(bodyParser.urlencoded({ limit: "500mb", extended: true}));
